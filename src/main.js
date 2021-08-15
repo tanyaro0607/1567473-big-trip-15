9929be5bd9c5ea5a3f68221dcc9d5e0870fb38a4
Tanya <ta.rohlova@yandex.ru>
2021-08-15 12:20:21 +0300

    реализую замену точки маршрута на форму редактирования

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import SiteMenuView from './view/site-menu.js'; //Меню
-import TripInfoSectionView from './view/trip-info-section.js'; // Cписок
-import TripInfoView from './view/trip-info.js'; //Маршрут и стоимость
-import TripInfoCostView from './view/trip-info-cost.js'; //Маршрут и стоимость
+import TripInfoSectionView from './view/trip-info-section.js'; // контейнер для маршрута и стоимости
+import TripInfoView from './view/trip-info.js'; //Маршрут
+import TripInfoCostView from './view/trip-info-cost.js'; //стоимость
 import FilterView from './view/filter.js'; //Фильтр
 import SortFormView from './view/form-sort.js'; //Сортировка
 import TripPointEditView from './view/form-edit-and-add.js'; //Форма редактирования
-import ListTripPointView from './view/form-list-trip-points'; // Cписок
-import TripPointView from './view/form-trip-point.js'; // Точки
-import {generateTripPoint} from './moсk/trip-point- mock.js';
+import ListTripPointView from './view/form-list-trip-points'; // контейнер для точек маршрута
+import TripPointView from './view/form-trip-point.js'; // Точки маршрута
+import {generateTripPoint} from './moсk/trip-point- mock.js'; //временные данные
 import {render, RenderPosition} from './utils.js';
 
 // console.log(generateTripPoint())
@@ -23,31 +23,48 @@ const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteEventsElement = document.querySelector('.trip-events');
 const TRIP_POINT_COUNT = 3;
 
+//отрисовка задач и формы редактирования
 const renderTripPoint = (tripPointListElement, point) => {
   const tripPointComponent = new TripPointView(point);
   const tripPointEditComponent = new TripPointEditView(point);
 
+  const replacePointToFormEdit = () => {
+    tripPointListElement.replaceChild(tripPointEditComponent.getElement(), tripPointComponent.getElement());
+  };
+
+  const replaceFormEditToPoint = () => {
+    tripPointListElement.replaceChild(tripPointComponent.getElement(), tripPointEditComponent.getElement());
+  };
+  //действия при клике на кнопку
+  tripPointComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replacePointToFormEdit();
+  });
+
+  tripPointEditComponent.getElement().querySelector('form').addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    replaceFormEditToPoint();
+  });
+
   render(tripPointListElement, tripPointComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
+// отрисовка контейнера для точек маршрута
 const listTripPointComponent = new ListTripPointView();
-render(siteEventsElement, listTripPointComponent.getElement(), RenderPosition.BEFOREEND); //Список
+render(siteEventsElement, listTripPointComponent.getElement(), RenderPosition.BEFOREEND);
 
-//Отрисовка точек
+//Отрисовка точек маршрута
 for (let i = 0; i < TRIP_POINT_COUNT; i++) {
   renderTripPoint(listTripPointComponent.getElement(), points[i]);
 }
 
-const tripInfoSectionComponent = new TripInfoSectionView();
+const tripInfoSectionComponent = new TripInfoSectionView(); //контейнер для маршрута и стоимости
 
-render(siteMainNavigationElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND); //Меню
-render(siteMainElement, tripInfoSectionComponent.getElement(), RenderPosition.AFTERBEGIN); //Маршрут и стоимость - список
+render(siteMainNavigationElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND); //отриосвка Меню
+render(siteMainElement, tripInfoSectionComponent.getElement(), RenderPosition.AFTERBEGIN); //отриосвка контейнера для маршрута и стоимости
+render(tripInfoSectionComponent.getElement(), new TripInfoView(points).getElement(), RenderPosition.AFTERBEGIN); //отриосвка Маршрута
+render(tripInfoSectionComponent.getElement(), new TripInfoCostView().getElement(), RenderPosition.BEFOREEND); //отриосвка стоимости
+render(siteFilterElement, new FilterView().getElement(), RenderPosition.BEFOREEND); //отриосвка Фильтра
+render(siteEventsElement, new SortFormView().getElement(), RenderPosition.AFTERBEGIN); //отриосвка Сортировки
+// render(listTripPointComponent.getElement(),new TripPointEditView(points[0]).getElement(), RenderPosition.AFTERBEGIN); //отриосвка формы Редактирования
 
-render(tripInfoSectionComponent.getElement(), new TripInfoView(points).getElement(), RenderPosition.AFTERBEGIN); //Маршрут и стоимость
-render(tripInfoSectionComponent.getElement(), new TripInfoCostView().getElement(), RenderPosition.BEFOREEND); //Маршрут и стоимость
-render(siteFilterElement, new FilterView().getElement(), RenderPosition.BEFOREEND); //Фильтр
-render(siteEventsElement, new SortFormView().getElement(), RenderPosition.AFTERBEGIN); //Сортировка
-render(listTripPointComponent.getElement(),new TripPointEditView(points[0]).getElement(), RenderPosition.AFTERBEGIN); //Редактирование
-
-//ф-я renderElement принимает три параметра: сслыку на контейнер, сам элемент, место - куда поместить //куда, что, где
 //new SiteMenuView().getElement() - создаем экземпляр класса через new и вызываем метод getElement

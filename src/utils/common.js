d9f5e8e5be531501a376d4499e1d9ac20d57fe92
Tanya <ta.rohlova@yandex.ru>
2021-09-27 22:46:31 +0300

    тест

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,4 +1,3 @@
-//ф-я, необходимая чтобы создать но основании html-разметки создать новый дом-элемент, который мы потом сможем куда-то вставить
 export const createElement = (template) => {
   const newElement = document.createElement('div'); //создаём div
   newElement.innerHTML = template; //и в него д обавляем ту разметку, которая пришла
